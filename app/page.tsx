d5e0fa33819f058b83f5e1421385133fc706eea2
Chainstrument <minitutoriel@gmail.com>
2024-09-07 19:53:06 +0200

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
      <div>
-      <h1 className="text-3xl underline">
+      <h1 className="text-white text-3xl underline">
         Welcome to CAre Pulse
       </h1>
      </div>
