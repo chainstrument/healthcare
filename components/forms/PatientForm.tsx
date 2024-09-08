89e08ba906bab500b336714becf0d8f47f233d5f
Chainstrument <minitutoriel@gmail.com>
2024-09-08 11:42:19 +0200

    add form organisation

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,16 +6,23 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form} from "@/components/ui/form"
+import CustomFormField from "./CustomFormField"
+
+export enum FormFieldType  {
+    INPUT = "input",
+    TEXTAREA = "textarea",
+    PHONE_INPUT = "phoneInput",
+    CHECKBOX = "checkbox",
+    DATE_PICKER = "datePicker",
+    TIME_PICKER = "timePicker",
+    FILE = "file",
+    CHECKBOX_GROUP = "checkboxGroup",
+    RADIO_GROUP = "radioGroup"
+    RADIO = "radio",
+    SELECT = "select",
+    SKELETON = "skeleton",
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -41,23 +48,22 @@ const PatientForm = () => {
   
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+            <h1  className="header"> Hi There </h1>
+            <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="name"
+            label="full name"
+            placeholder="John Doe"
+            iconSrc="assets/incons/user.svg"
+            iconAlt="user"
+
         />
+        
         <Button type="submit">Submit</Button>
       </form>
     </Form>
